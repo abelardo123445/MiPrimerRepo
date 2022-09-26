ae1901a764b4707d577bb9b84e76c7758e2998cf
abelardo123445 <112274443+abelardo123445@users.noreply.github.com>
2022-09-26 11:03:30 -0500

    Add files via upload

---
+++ 4-main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int num1, num2, num1_abs, num2_abs, producto;
+
+
+    printf("Calculador de productos (sin usar el operador *)\n\n");
+    printf("Introduzca el primer numero: ");
+    scanf("%d", &num1);
+    printf("Introduzca el segundo numero: ");
+    scanf("%d", &num2);
+
+
+    if(num1<0)
+        num1_abs=num1*(-1);
+    else
+        num1_abs=num1;
+
+
+    if(num2<0)
+        num2_abs=num2*(-1);
+    else
+        num2_abs=num2;
+
+
+    for(9
+    {
+        "Esto no lo consigo hacer"
+    }
+
+
+    if(num1*num2<0)
+        producto*=-1;
+
+
+    printf("%d * %d = %d", num1, num2, producto);
+
+
+    return 0;
+}
